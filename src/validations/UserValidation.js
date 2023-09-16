195ff4d9e61fd3368b41c342e1c98809020b16ca
Alleyah Manalili <21100049@usc.edu.ph>
2023-09-16 20:46:11 +0800

    Update UserValidation.js Error Message

--- src/validations/UserValidation.js
+++ src/validations/UserValidation.js
@@ -1,12 +1,12 @@
 import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
-    fname: yup.string().required(),
-    lname: yup.string().required(),
-    age: yup.string().required().matches(/^(0?[1-9]|[1-9][0-9]|[1][0-9]{2})$/, "Invalid age!"),
-    gender: yup.string().required(),
-    email: yup.string().email().required().matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email!"),
-    password: yup.string().min(8).required().matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least 1 number and uppercase letter!"),
-    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords do not match!"),
+    fname: yup.string().required("First name is required!"),
+    lname: yup.string().required("Last name is required!"),
+    age: yup.string().required("Age is required!").matches(/^(0?[1-9]|[1-9][0-9]|[1][0-9]{2})$/, "Invalid age!"),
+    gender: yup.string().required("Gender is required!"),
+    email: yup.string().email().required("Email is required!").matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email!"),
+    password: yup.string().min(8, "Password must be at least 8 characters!").required().matches(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{8,}$/, "Password must contain at least 1 number and uppercase letter!"),
+    confirmPassword: yup.string().required("Confirm Password is required!").oneOf([yup.ref("password"), null], "Passwords do not match!"),
     terms: yup.boolean().oneOf([true], 'You must accept the terms and conditions!')
 })
